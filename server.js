72dde045d9ae60f823bff49b41e926cc14c5e2a3
Sajid Hasan <sajid.hasan@cefalo.com>
2022-10-06 15:07:05 +0600

    fifthkskdhj

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const app = require('./index');
 
 const PORT = process.env.PORT || 3000;
