07b110d2741f59ea13f133ce60a7fed6c6bf1ba2
Sajid Hasan <sajid.hasan@cefalo.com>
2022-10-06 15:19:01 +0600

    dkls

--- utils/ErrorHandler.js
+++ utils/ErrorHandler.js
@@ -26,7 +26,7 @@ const errorHandler = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
     console.log(`hello ${err.statusCode} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
-
+    //sendErrorDev(err, res);
     if (process.env.NODE_ENV === 'development') {
       sendErrorDev(err, res);
     } else if (process.env.NODE_ENV === 'production') {
